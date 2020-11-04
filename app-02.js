3aad4e30de2c60d6c0b24d6291b7cd9210cdb9df
booldook <booldook@gmail.com>
2020-11-04 10:30:32 +0900

    scss

--- app-02.js
+++ app-02.js
@@ -13,3 +13,16 @@ app.listen(3000, () => {
 /** 라우터 *******************************/
 app.use('/', express.static(path.join(__dirname, './public')));
 
+app.get('/', (req, res) => {
+	var q = req.query.q;
+	res.send(`응답: ${q}`);
+});
+
+app.get('/:userID/:listID', (req, res) => {
+	const content = ['[홍길동전] 아버지를 아버지라... ', '[별주부전] 용왕이 거북이의 간을...']
+	var html = `
+		<h1>${req.params.userID} 블로그 입니다.</h1>
+		<h2>${req.params.listID}번글 입니다.</h2>
+		<p>${content[req.params.listID]}</p>`;
+	res.send(html);
+});
