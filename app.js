cc5526d84e4dceb1b8d558767dc0c161c8067892
booldook <booldook@gmail.com>
2020-11-04 13:16:58 +0900

    ...

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const books = [
+	{id: 1, title: '별주부전', writer: '겁없는 토끼', content: '용왕의 사주를 받은 거북이가 나를.'},
+	{id: 2, title: '심청전', writer: '심청이 아빠', content: '내딸이 가출을...'},
+	{id: 3, title: '춘향전', writer: '변사또', content: '춘향이와 이몽룡이 눈이 맞아...'},
+];
+
 /** 서버구동 *******************************/
 app.listen(3000, () => {
 	console.log("=====================");
@@ -17,19 +23,27 @@ app.locals.pretty = true;
 
 /** 라우터 *******************************/
 app.use('/', express.static(path.join(__dirname, './public')));
-app.get('/sample', (req, res) => {
+app.get('/list', (req, res) => {
 	const pug = {
 		title: {
 			head: '도서 리스트',
 			body: '고전소설 도서 목록',
 			small: '작자 미상의 고전 소설'
 		},
-		lists: [
-			{id: 1, title: '별주부전', writer: '겁없는 토끼', content: '용왕의 사주를 받은 거북이가 나를.'},
-			{id: 2, title: '심청전', writer: '심청이 아빠', content: '내딸이 가출을...'},
-			{id: 3, title: '춘향전', writer: '변사또', content: '춘향이와 이몽룡이 눈이 맞아...'},
-		]
+		lists: [...books]
 	};
-	res.render('sample', pug);
+	res.render('book/list', pug);
+});
+
+app.get('/book/:id', (req, res) => {
+	const pug = {
+		title: {
+			head: '도서 리스트',
+			body: '고전소설 도서 목록',
+			small: '작자 미상의 고전 소설'
+		},
+		lists: [...books]
+	};
+	res.render('book/view', pug);
 });
 
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -6,8 +6,24 @@ var express = require('express');
 var app = express();
 
 var path = require('path');
-/** 서버구동 *******************************/
 
+var books = [{
+  id: 1,
+  title: '별주부전',
+  writer: '겁없는 토끼',
+  content: '용왕의 사주를 받은 거북이가 나를.'
+}, {
+  id: 2,
+  title: '심청전',
+  writer: '심청이 아빠',
+  content: '내딸이 가출을...'
+}, {
+  id: 3,
+  title: '춘향전',
+  writer: '변사또',
+  content: '춘향이와 이몽룡이 눈이 맞아...'
+}];
+/** 서버구동 *******************************/
 
 app.listen(3000, function () {
   console.log("=====================");
@@ -22,29 +38,25 @@ app.locals.pretty = true;
 /** 라우터 *******************************/
 
 app.use('/', express["static"](path.join(__dirname, './public')));
-app.get('/sample', function (req, res) {
+app.get('/list', function (req, res) {
   var pug = {
     title: {
       head: '도서 리스트',
       body: '고전소설 도서 목록',
       small: '작자 미상의 고전 소설'
     },
-    lists: [{
-      id: 1,
-      title: '별주부전',
-      writer: '겁없는 토끼',
-      content: '용왕의 사주를 받은 거북이가 나를.'
-    }, {
-      id: 2,
-      title: '심청전',
-      writer: '심청이 아빠',
-      content: '내딸이 가출을...'
-    }, {
-      id: 3,
-      title: '춘향전',
-      writer: '변사또',
-      content: '춘향이와 이몽룡이 눈이 맞아...'
-    }]
+    lists: [].concat(books)
   };
-  res.render('sample', pug);
+  res.render('book/list', pug);
+});
+app.get('/book/:id', function (req, res) {
+  var pug = {
+    title: {
+      head: '도서 리스트',
+      body: '고전소설 도서 목록',
+      small: '작자 미상의 고전 소설'
+    },
+    lists: [].concat(books)
+  };
+  res.render('book/view', pug);
 });
